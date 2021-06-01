a7ab19df68b99949a43d6b2a1fe7e4134b33ec3a
Lämppi Lütti <lampputti@mailfence.com>
2021-06-02 01:19:49 +0300

    Refactor clojure code

--- src/clojure/tdlib_json/core.clj
+++ src/clojure/tdlib_json/core.clj
@@ -1,48 +1,51 @@
 (ns tdlib-json.core
-  (:require [cheshire.core :as che]
-            [clojure.core.async :as async])
-  (:import [tdlib_json TgClient]))
+  (:require
+   [cheshire.core :as json]
+   [clojure.core.async :as async])
+  (:import [tdlib_json TgJsonClient]))
+
+
+(defn jsonify [messageMap]
+  (json/generate-string messageMap))
+
+(defn json-parse [string]
+  (json/parse-string string true))
+
 
 (def client (atom nil))
 
-(defn create-client [path-to-lib timeout]
-  (TgClient. path-to-lib timeout))
-
 (def message-queue (atom nil))
 
-(defn jsonify [messageMap] (che/generate-string messageMap))
 
-(defn json-parse [string]
-  (che/parse-string string true))
+(defn create-client [path-to-lib verbosity]
+  (TgJsonClient. path-to-lib verbosity))
 
-(defn client-execute
-  ([messageMap] (. @client execute (jsonify messageMap))))
+(defn client-send [messageMap]
+  (.send @client (jsonify messageMap)))
 
-(defn client-receive
-  []
-  (json-parse (. @client receive)))
+(defn client-execute [messageMap]
+  (.execute @client (jsonify messageMap)))
+
+(defn client-receive [timeout]
+  (json-parse (.receive @client timeout)))
 
 (defn client-destroy []
+  (.destroy @client)
   (reset! message-queue nil)
   (reset! client nil))
 
-(defn client-send
-  [messageMap]
-  (. @client send (jsonify messageMap)))
-
 (defn client-start
-  ([path-to-lib timeout verbosity-level]
-   (client-start path-to-lib timeout verbosity-level 1024))
-  ([path-to-lib timeout verbosity-level buffer-size]
+  ([path-to-lib verbosity-level]
+   (client-start path-to-lib verbosity-level 1024))
+  ([path-to-lib verbosity-level buffer-size]
    (reset! message-queue (async/chan (async/buffer buffer-size)))
-   (reset! client (create-client path-to-lib timeout))
-   (. @client startClient verbosity-level)))
+   (reset! client (create-client path-to-lib verbosity-level))))
 
-(defn init-reader-loop []
-  (async/go-loop []
+
+(defn init-reader-loop [timeout]
+  (async/go-loop [t timeout]
     (when (and @message-queue @client)
-      (let [message (client-receive)]
-        (when-not
-          (nil? message)
+      (let [message (client-receive timeout)]
+        (when-not (nil? message)
           (async/>! @message-queue message))
-        (recur)))))
+        (recur t)))))
