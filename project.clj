9c6d8da437bb897813da4712f1249f8412a9946e
Lämppi Lütti <lampputti@mailfence.com>
2021-06-01 04:17:05 +0300

    Make clojure dev dependency

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/MityaSaray/clojure-tdlib-json"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.sun.jna/jna "3.0.9"]
+  :dependencies [[com.sun.jna/jna "3.0.9"]
                  [cheshire "5.8.1"]
                  [org.clojure/core.async "0.4.490"]]
   :java-source-paths ["src/main/java"]
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[org.clojure/clojure "1.10.0"]]}})
